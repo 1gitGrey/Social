04ed26fbfae2f63920a6f93729faa7fa7d8f0ca6
Grey Grissom <grey.grissom@aya.yale.edu>
2017-10-14 07:00:23 -0700

    ui

---
+++ Social/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Social
+//
+//  Created by Greyson Grissom on 14//10//17.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY = CGFloat(120.0/255.0)
+
---
+++ Social/Views/FancyView.swift
@@ -0,0 +1,21 @@
+//
+//  FancyView.swift
+//  Social
+//
+//  Created by Greyson Grissom on 14//10//17.
+//
+
+import UIKit
+
+class FancyView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.67).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+    }
+}
